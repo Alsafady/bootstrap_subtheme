7a910d6a7092ffad4a8c5bc1cb2af4a8813202e9
Abdulaziz Alsafadi <iweb1700@hotmail.com>
2019-03-03 12:36:58 +0300

    add fonts & others

--- README.md
+++ README.md
@@ -1,8 +1,33 @@
 
+In Your theme Folder
 
 git clone https://github.com/Alsafady/drupal8_bootstrap_subtheme.git
 
-git pull https://github.com/Alsafady/drupal8_bootstrap_subtheme.git
+
+
+
+Features:
+
+RTL Support
+
+RTL Or LTR Custom
+
+Arabic Fonts by google webfonts & fontface.me
+
+English Fonts
+
+Css Custom for Common Drupal Elements & Bootstrap
+
+Css Colors links
+
+WOW js & Animate css
+
+Fontawesome Included
+
+Contain views twig Files
+
+backtotop arrow
+
 
 
 
@@ -18,3 +43,107 @@ https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.css
 https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js
 
 https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.css
+
+
+
+
+All animate classes:
+bounce
+flash
+pulse
+rubberBand
+shake
+headShake
+swing
+tada
+wobble
+jello
+bounceIn
+bounceInDown
+bounceInLeft
+bounceInRight
+bounceInUp
+bounceOut
+bounceOutDown
+bounceOutLeft
+bounceOutRight
+bounceOutUp
+fadeIn
+fadeInDown
+fadeInDownBig
+fadeInLeft
+fadeInLeftBig
+fadeInRight
+fadeInRightBig
+fadeInUp
+fadeInUpBig
+fadeOut
+fadeOutDown
+fadeOutDownBig
+fadeOutLeft
+fadeOutLeftBig
+fadeOutRight
+fadeOutRightBig
+fadeOutUp
+fadeOutUpBig
+flipInX
+flipInY
+flipOutX
+flipOutY
+lightSpeedIn
+lightSpeedOut
+rotateIn
+rotateInDownLeft
+rotateInDownRight
+rotateInUpLeft
+rotateInUpRight
+rotateOut
+rotateOutDownLeft
+rotateOutDownRight
+rotateOutUpLeft
+rotateOutUpRight
+hinge
+jackInTheBox
+rollIn
+rollOut
+zoomIn
+zoomInDown
+zoomInLeft
+zoomInRight
+zoomInUp
+zoomOut
+zoomOutDown
+zoomOutLeft
+zoomOutRight
+zoomOutUp
+slideInDown
+slideInLeft
+slideInRight
+slideInUp
+slideOutDown
+slideOutLeft
+slideOutRight
+slideOutUp
+heartBeat
+
+
+
+
+you can add also:
+
+.yourElement {
+  animation-duration: 3s;
+  animation-delay: 2s;
+  animation-iteration-count: infinite;
+}
+
+
+Or by
+
+<section class="wow slideInLeft" data-wow-duration="2s" data-wow-delay="5s">
+
+</section>
+
+<section class="wow slideInRight" data-wow-offset="10"  data-wow-iteration="10">
+
+</section>
--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,30 @@
+//    WOW JS
+jQuery(document).ready(function($){
+    if (typeof WOW === 'function') {
 
+      var wow = new WOW(
+        {
+          boxClass:     'wow',      // animated element css class (default is wow)
+          animateClass: 'animated', // animation css class (default is animated)
+          offset:       0,          // distance to the element when triggering the animation (default is 0)
+          mobile:       true,       // trigger animations on mobile devices (default is true)
+          live:         true,       // act on asynchronously loaded content (default is true)
+          callback:     function(box) {
+            // the callback is fired every time an animation is started
+            // the argument that is passed in is the DOM node being animated
+          },
+          scrollContainer: null // optional scroll container selector, otherwise use window
+        }
+      );
+      wow.init();
+
+    }
+  });
+
+
+
+
+//    backtotop arrow
 jQuery(document).ready(function () {
 
 
@@ -19,34 +45,4 @@ jQuery(document).ready(function () {
     });
    //  End Scroll to To
 
-
 });
-
-
-
-
-jQuery(document).ready(function($){
-    if (typeof WOW === 'function') {
-
-
-
-      var wow = new WOW(
-        {
-          boxClass:     'wow',      // animated element css class (default is wow)
-          animateClass: 'animated', // animation css class (default is animated)
-          offset:       0,          // distance to the element when triggering the animation (default is 0)
-          mobile:       true,       // trigger animations on mobile devices (default is true)
-          live:         true,       // act on asynchronously loaded content (default is true)
-          callback:     function(box) {
-            // the callback is fired every time an animation is started
-            // the argument that is passed in is the DOM node being animated
-          },
-          scrollContainer: null // optional scroll container selector, otherwise use window
-        }
-      );
-      wow.init();
-
-
-
-    }
-  });
